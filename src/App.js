542c569302b50957059d5099654b79fe8a2671af
Peter Li <tyuo9980@gmail.com>
2017-11-09 04:15:40 -0800

    api test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+      awef:[]
+  }
+
+  componentDidMount(){
+    fetch('/test')
+      .then(res => {
+        return res.json()
+      })
+      .then(awef => {
+        this.setState({awef})
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1> WHA </h1>
+        {this.state.awef.map(user =>
+          <div key={user.id}>{user.username}</div>
+        )}
       </div>
     );
   }
