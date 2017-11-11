1a95fef8a4e0d220ae2b02dcb2ff5cfa597b6ff3
Peter Li <pli@athinkingape.com>
2017-11-10 21:50:01 -0800

    homepage

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 
 import Routes from './routes';
 
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './timewinder.css';
+
 ReactDOM.render(
   <Routes />,
   document.getElementById('root')
