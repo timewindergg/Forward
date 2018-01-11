fe3710927bfb44e9391a9ebd0102e58139019824
NullCodex <jamesonyu95@gmail.com>
2018-01-10 22:59:00 -0500

    added moment support and queue id

---
+++ src/shared/queueIdMappings.js
@@ -0,0 +1,118 @@
+const QueueIdMappings = {
+    70: {
+        name: "One for All"
+    },
+    72: {
+        name: "1v1 Snowdown"
+    },
+    73: {
+        name: "2v2 Snowdown"
+    },
+    75: {
+        name: "Hexakill"
+    },
+    76: {
+        name: "URF"
+    },
+    78: {
+        name: "One For All"
+    },
+    83: {
+        name: "URF AI"
+    },
+    98: {
+        name: "Hexakill"
+    },
+    100: {
+        name: "ARAM"
+    },
+    310: {
+        name: "Nemesis"
+    },
+    313: {
+        name: "Black Market Brawlers"
+    },
+    317: {
+        name: "Definitely Not Dominion"
+    },
+    325: {
+        name: "All Random"
+    },
+    400: {
+        name: "5v5 Draft"
+    },
+    420: {
+        name: "Ranked Solo"
+    },
+    430: {
+        name: "Blind Pick"
+    },
+    440: {
+        name: "Ranked Flex"
+    },
+    450: {
+        name: "ARAM"
+    },
+    460: {
+        name: "Blind Pick"
+    },
+    470: {
+        name: "Ranked Flex"
+    },
+    600: {
+        name: "Blood Hunt"
+    },
+    610: {
+        name: "Dark Star"
+    },
+    800: {
+        name: "Co-op vs. AI"
+    },
+    810: {
+        name: "Co-op vs. AI"
+    },
+    820: {
+        name: "Co-op vs. AI"
+    },
+    830: {
+        name: "Co-op vs. AI"
+    },
+    840: {
+        name: "Co-op vs. AI"
+    },
+    850: {
+        name: "Co-op vs. AI"
+    },
+    900: {
+        name: "ARURF"
+    },
+    910: {
+        name: "Ascension"
+    },
+    920: {
+        name: "Poro King"
+    },
+    940: {
+        name: "Nexus Siege"
+    },
+    950: {
+        name: "Doom Bots"
+    },
+    960: {
+        name: "Doom Bots"
+    },
+    980: {
+        name: "Star Guardian"
+    },
+    990: {
+        name: "Star Guardian"
+    },
+    1000: {
+        name: "PROJECT"
+    },
+    1010: {
+        name: "ARURF"
+    }
+};
+
+export default QueueIdMappings;
