0736232d6b91ad42f6fc706da66df6c6481fcd94
Peter Li <tyuo9980@gmail.com>
2018-03-09 14:28:21 -0500

    discord and splash art creds

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -6,10 +6,21 @@ class Footer extends Component{
   render(){
     return(
       <div className="footer">
+        <div className="credits">
+          <div className="artCredits">
+            Art by&nbsp;
+            <a href="https://pwang347.github.io">Paul Wang</a>
+          </div>
+        </div>
         <div className="disclaimer">
           <span>
             timewinder.gg isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.
           </span>
+        </div>
+        <div className="social">
+          <a href="https://discord.gg/pT78Nz6">
+            <i class="fab fa-discord"></i>
+          </a>
         </div>
       </div>
     );
